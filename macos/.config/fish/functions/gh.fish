4da82396776bc0daec0360ed38d73305eb9ac17f
k-yamamoto <k.yamamoto@fenrir.co.jp>
2019-02-10 19:09:17 +0900

    Fix: gh and gr command not support escape

--- macos/.config/fish/functions/gh.fish
+++ macos/.config/fish/functions/gh.fish
@@ -1,5 +1,9 @@
 function gh
- ghq list | fzf --select-1 --exit-0 | read repo_name
+  ghq list | fzf --select-1 --exit-0 | read repo_name
 
- open "https://$repo_name"
+  if [ ! $repo_name ]
+    return 
+  end
+
+  open "https://$repo_name"
 end
--- macos/.config/fish/functions/gr.fish
+++ macos/.config/fish/functions/gr.fish
@@ -1,5 +1,9 @@
 function gr
   ghq list | fzf --select-1 --exit-0 | read repo_name
 
+  if [ ! $repo_name ]
+    return
+  end
+
   ghq look $repo_name
 end
