2f0fa7be8142d4ff04970f7e1970337aa31aff6a
k-ymmt <account@kymmt.net>
2022-02-22 20:00:12 +0900

    change .sh to Brewfile

--- macos/install.sh
+++ macos/install.sh
@@ -2,10 +2,4 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install \
-  ranger \
-  ghq \
-  tmux \
-  direnv \
-  neovim \
-  jesseduffield/lazygit/lazygit
+brew bundle
