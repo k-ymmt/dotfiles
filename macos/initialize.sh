15f4f6ec362f2119bb1a7885fffa563d7d663008
Kazuki Yamamoto <account@kymmt.net>
2024-04-05 14:45:19 +0900

    initialize.sh

--- macos/initialize.sh
+++ macos/initialize.sh
@@ -3,8 +3,11 @@
 dir="$(cd "$(dirname $0)";pwd)"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+export PATH:$PATH:/opt/homebrew/bin
 
 brew bundle
 
 "$dir/link.sh"
 "$dir/plugin_install_vim.sh"
+
+defaults write -g NSUserKeyEquivalents -dict-add 'Minimize' '\0'
